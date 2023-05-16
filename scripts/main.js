7065b70b18a83facc216d2921bf139e63c003a08
PHYC <PhilHYChen@users.noreply.github.com>
2023-05-16 14:17:37 +0800

    Restructuring HTML. and minor style/script fixes.

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,10 @@ document.getElementById("darkMode").addEventListener("click", () => {
 
 function toggleDarkModeOn() {
     // Unit test:
-    // alert("Light Mode");
+    // alert("Dark Mode");
     document.querySelectorAll("a").forEach(element => element.style.color = "#bbb");
     document.querySelector("body").style.backgroundColor = "#222";
-    document.querySelector("body").style.color = "white";
+    document.querySelector("body").style.color = "#fff";
     document.querySelector("body").style.fontWeight = "300"
     document.querySelector("button").style.backgroundColor = "#222";
     document.querySelector("button").style.borderColor = "#ccc";
@@ -25,12 +25,12 @@ function toggleDarkModeOn() {
 
 function toggleDarkModeOff() {
     // Unit test:
-    // alert("Dark Mode");
+    // alert("White Mode");
     document.querySelectorAll("a").forEach(element => element.style.color = "#666");
-    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("body").style.backgroundColor = "#fff";
     document.querySelector("body").style.color = "#222";
     document.querySelector("body").style.fontWeight = "400"
-    document.querySelector("button").style.backgroundColor = "white";
+    document.querySelector("button").style.backgroundColor = "#eee";
     document.querySelector("button").style.borderColor = "#444";
     document.querySelector("button").style.color = "#444";
     document.querySelector("button").textContent = "以深色模式閱覽";
