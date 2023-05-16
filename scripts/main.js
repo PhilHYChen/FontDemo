600701694d22e67c6ff9e5020545c5bd6b91158f
PHYC <PhilHYChen@users.noreply.github.com>
2023-05-17 01:04:15 +0800

    Add system color theme preference detection.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,40 @@
+// Dark Mode Default Button Text
+if (window.matchMedia("prefers-color-scheme: dark")) {
+    document.getElementById("toggleDarkMode").textContent = "關閉深色模式"
+} else {
+    document.getElementById("toggleDarkMode").textContent = "開啟深色模式"
+}
+
 // Dark Mode Toggle
 document.getElementById("toggleDarkMode").addEventListener("click", () => {
     // Unit test:
     // alert("Invoked Successfully.");
     // alert(getComputedStyle(document.body).backgroundColor);
-    if (getComputedStyle(document.body).backgroundColor == "rgb(255, 255, 255)") {
-        toggleDarkModeOn();
+    if (window.matchMedia("prefers-color-scheme: dark")) {
+        toggleLightMode();
     } else {
-        toggleDarkModeOff();
+        toggleDarkMode();
     }
 });
 
-function toggleDarkModeOn() {
+function toggleLightMode() {
     // Unit test:
-    // alert("Dark Mode");
-    document.querySelector("body").classList.add("darkMode");
-    document.querySelector("button").classList.add("darkMode");
+    // alert("Light Mode");
+    document.querySelectorAll("a[href]").forEach(element => {element.classList.toggle("lightMode")});
+    document.querySelector("body").classList.toggle("lightMode");
+    document.querySelector("button").classList.toggle("lightMode");
+    document.getElementById("toggleDarkMode").textContent = 
+        (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
+        "開啟深色模式" : "關閉深色模式";
 }
 
-function toggleDarkModeOff() {
+function toggleDarkMode() {
     // Unit test:
-    // alert("White Mode");
-    document.querySelector("body").classList.remove("darkMode");
-    document.querySelector("button").classList.remove("darkMode");
+    // alert("Dark Mode");
+    document.querySelectorAll("a[href]").forEach(element => {element.classList.toggle("darkMode")});
+    document.querySelector("body").classList.toggle("darkMode");
+    document.querySelector("button").classList.toggle("darkMode");
+    document.getElementById("toggleDarkMode").textContent = 
+    (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
+    "開啟深色模式" : "關閉深色模式"
 }
