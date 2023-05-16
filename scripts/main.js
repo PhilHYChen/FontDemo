7beace54058822b24ff9aeb5872bb9dff8a769d3
PHYC <PhilHYChen@users.noreply.github.com>
2023-05-16 18:34:47 +0800

    Rewrite Dark Mode by adding/removing class attributes to elements.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // Dark Mode Toggle
-document.getElementById("darkMode").addEventListener("click", () => {
+document.getElementById("toggleDarkMode").addEventListener("click", () => {
     // Unit test:
     // alert("Invoked Successfully.");
     // alert(getComputedStyle(document.body).backgroundColor);
@@ -13,25 +13,13 @@ document.getElementById("darkMode").addEventListener("click", () => {
 function toggleDarkModeOn() {
     // Unit test:
     // alert("Dark Mode");
-    document.querySelectorAll("a").forEach(element => element.style.color = "#bbb");
-    document.querySelector("body").style.backgroundColor = "#222";
-    document.querySelector("body").style.color = "#fff";
-    document.querySelector("body").style.fontWeight = "300"
-    document.querySelector("button").style.backgroundColor = "#222";
-    document.querySelector("button").style.borderColor = "#ccc";
-    document.querySelector("button").style.color = "#ccc";
-    document.querySelector("button").textContent = "以淺色模式閱覽";
+    document.querySelector("body").classList.add("darkMode");
+    document.querySelector("button").classList.add("darkMode");
 }
 
 function toggleDarkModeOff() {
     // Unit test:
     // alert("White Mode");
-    document.querySelectorAll("a").forEach(element => element.style.color = "#666");
-    document.querySelector("body").style.backgroundColor = "#fff";
-    document.querySelector("body").style.color = "#222";
-    document.querySelector("body").style.fontWeight = "400"
-    document.querySelector("button").style.backgroundColor = "#eee";
-    document.querySelector("button").style.borderColor = "#444";
-    document.querySelector("button").style.color = "#444";
-    document.querySelector("button").textContent = "以深色模式閱覽";
+    document.querySelector("body").classList.remove("darkMode");
+    document.querySelector("button").classList.remove("darkMode");
 }
