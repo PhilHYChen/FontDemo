52e3e615f56d2938d084ff7d9d14faa62ea51126
PHYC <PhilHYChen@users.noreply.github.com>
2023-05-18 03:24:02 +0800

    Add optional grid layout for font display.

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ function toggleLightMode() {
     // alert("Light Mode");
     document.querySelectorAll("a[href]").forEach(element => {element.classList.toggle("lightMode")});
     document.querySelector("body").classList.toggle("lightMode");
-    document.querySelector("button").classList.toggle("lightMode");
+    document.querySelectorAll("button").forEach(element => element.classList.toggle("lightMode"));
     document.getElementById("toggleDarkMode").textContent = 
         (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
         "開啟深色模式" : "關閉深色模式";
@@ -33,8 +33,19 @@ function toggleDarkMode() {
     // alert("Dark Mode");
     document.querySelectorAll("a[href]").forEach(element => {element.classList.toggle("darkMode")});
     document.querySelector("body").classList.toggle("darkMode");
-    document.querySelector("button").classList.toggle("darkMode");
+    document.querySelectorAll("button").forEach(element => element.classList.toggle("darkMode"));
     document.getElementById("toggleDarkMode").textContent = 
-    (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
-    "開啟深色模式" : "關閉深色模式"
+        (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
+        "開啟深色模式" : "關閉深色模式"
 }
+
+// Font Display List/Grid Toggle
+document.getElementById("toggleListGridView").addEventListener("click", () => {
+    // Unit test:
+    // alert("Grid View");
+    document.getElementById("fontDisplay").classList.toggle("listView");
+    document.getElementById("fontDisplay").classList.toggle("gridView");
+    document.getElementById("toggleListGridView").textContent = 
+        (document.getElementById("toggleListGridView").textContent === "格狀檢視") ?
+        "列表檢視" : "格狀檢視"
+});
