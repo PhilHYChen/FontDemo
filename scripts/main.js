bffb15b49c8d7676acc84ad57ec12d84e9bc3cb6
PHYC <PhilHYChen@users.noreply.github.com>
2023-05-18 21:38:08 +0800

    Add an input field for users to customize texts for display.

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ if (window.matchMedia("prefers-color-scheme: dark")) {
     document.getElementById("toggleDarkMode").textContent = "關閉深色模式"
 } else {
     document.getElementById("toggleDarkMode").textContent = "開啟深色模式"
-}
+};
 
 // Dark Mode Toggle
 document.getElementById("toggleDarkMode").addEventListener("click", () => {
@@ -23,10 +23,11 @@ function toggleLightMode() {
     document.querySelectorAll("a[href]").forEach(element => {element.classList.toggle("lightMode")});
     document.querySelector("body").classList.toggle("lightMode");
     document.querySelectorAll("button").forEach(element => element.classList.toggle("lightMode"));
+    document.querySelectorAll("input").forEach(element => element.classList.toggle("lightMode"));
     document.getElementById("toggleDarkMode").textContent = 
         (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
         "開啟深色模式" : "關閉深色模式";
-}
+};
 
 function toggleDarkMode() {
     // Unit test:
@@ -34,12 +35,13 @@ function toggleDarkMode() {
     document.querySelectorAll("a[href]").forEach(element => {element.classList.toggle("darkMode")});
     document.querySelector("body").classList.toggle("darkMode");
     document.querySelectorAll("button").forEach(element => element.classList.toggle("darkMode"));
+    document.querySelectorAll("input").forEach(element => element.classList.toggle("darkMode"));
     document.getElementById("toggleDarkMode").textContent = 
         (getComputedStyle(document.body).backgroundColor === "rgb(255, 255, 255)") ?
         "開啟深色模式" : "關閉深色模式"
-}
+};
 
-// Font Display List/Grid Toggle
+// Font Display: List/Grid Toggle
 document.getElementById("toggleListGridView").addEventListener("click", () => {
     // Unit test:
     // alert("Grid View");
@@ -49,3 +51,17 @@ document.getElementById("toggleListGridView").addEventListener("click", () => {
         (document.getElementById("toggleListGridView").textContent === "格狀檢視") ?
         "列表檢視" : "格狀檢視"
 });
+
+// Font Display: Change Display Text
+document.getElementById("changeDisplayText").addEventListener("input", () => {
+    document.querySelectorAll("#fontDisplay dd").forEach(
+        element => element.textContent =
+        document.getElementById("changeDisplayText").value.toString());
+    }
+);
+document.getElementById("changeDisplayText").addEventListener("focusout", () => {
+    if (document.getElementById("changeDisplayText").value === "")
+    document.querySelectorAll("#fontDisplay dd").forEach(
+        element => element.textContent = "法律之前人人平等，並有權享受法律的平等保護，不受任何歧視。人人有權享受平等保護，以免受違反本宣言的任何歧視行為以及煽動這種歧視的任何行為之害。")
+    }
+);
