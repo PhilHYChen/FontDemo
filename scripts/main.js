1d5d4fa8f47be97f17bbb8f4503c2ef81c5a9660
PHYC <PhilHYChen@users.noreply.github.com>
2023-05-24 04:09:49 +0800

    A slide bar is added for font size selection.

--- scripts/main.js
+++ scripts/main.js
@@ -43,72 +43,154 @@ function toggleDarkMode() {
         "開啟深色模式" : "關閉深色模式"
 };
 
-// Font Display: List/Grid Toggle
-document.getElementById("toggleListGridView").addEventListener("click", () => {
+// Font Demo: List/Grid Toggle
+document.getElementById("listGridToggle").addEventListener("click", () => {
     // Unit test:
     // alert("Grid View");
-    document.getElementById("fontDisplay").classList.toggle("listView");
-    document.getElementById("fontDisplay").classList.toggle("gridView");
-    document.getElementById("toggleListGridView").textContent = 
-        (document.getElementById("toggleListGridView").textContent === "格狀檢視") ?
+    document.getElementById("fontDemo").classList.toggle("listView");
+    document.getElementById("fontDemo").classList.toggle("gridView");
+    document.getElementById("listGridToggle").textContent = 
+        (document.getElementById("listGridToggle").textContent === "格狀檢視") ?
         "列表檢視" : "格狀檢視"
 });
 
-// Font Display: Modify Display Text
-document.getElementById("modDisplayText").addEventListener("input", () => {
-    document.querySelectorAll("#fontDisplay dd").forEach(
+// Font Demo: Custom Text Input
+document.getElementById("customTextInput").addEventListener("input", () => {
+    document.querySelectorAll("#fontDemo dd").forEach(
         element => element.textContent =
-        document.getElementById("modDisplayText").value.toString());
+        document.getElementById("customTextInput").value.toString());
 });
-document.getElementById("modDisplayText").addEventListener("focusout", () => {
-    if (document.getElementById("modDisplayText").value === "")
-    document.querySelectorAll("#fontDisplay dd").forEach(
+document.getElementById("customTextInput").addEventListener("focusout", () => {
+    if (document.getElementById("customTextInput").value === "")
+    document.querySelectorAll("#fontDemo dd").forEach(
         element => element.textContent = "法律之前人人平等，並有權享受法律的平等保護，不受任何歧視。人人有權享受平等保護，以免受違反本宣言的任何歧視行為以及煽動這種歧視的任何行為之害。")
 });
 
-// Font Display: Modify Display Font Size
+// Font Demo: Font Size Controllers
 
     // Set initial display size auto-select:
     let isSelected = false;
 
-        // If a matching option is present, it is selected.
-        document.querySelectorAll("#modDisplayFontSize option").forEach(
+        /* Dropdown Menu: if a matching option is present, it is selected. */
+        document.querySelectorAll("#fontSizeDropdownMenu option").forEach(
             element => {
                 if (element.textContent === window.getComputedStyle(document.body).getPropertyValue("font-size")) {
-                    element.setAttribute("selected", true);
+                    element.selected = true;
                     isSelected = true;
                 }
             }
         )
-        /* If no matching option is present, a new option is:
+        /* Dropdown Menu: if no matching option is present, a new option is:
         created and added to the dropdown menu,
-        assigned text content with computed font size,
+        assigned text content with computed font size of <body>,
         selected and
         sorted. */
         if (isSelected === false) {
-            let newOption = document.getElementById("modDisplayFontSize").appendChild(document.createElement("option"));
-            newOption.textContent = window.getComputedStyle(document.body).getPropertyValue("font-size");
-            newOption.setAttribute("selected", true);
+            let dynamicOption = document.getElementById("fontSizeDropdownMenu").appendChild(document.createElement("option"));
+            dynamicOption.textContent = window.getComputedStyle(document.body).getPropertyValue("font-size");
+            dynamicOption.selected = true;
             let optionArray = Array.from(
-                document.getElementById("modDisplayFontSize").options
+                document.getElementById("fontSizeDropdownMenu").options
             );
             // Unit test:
             // alert(optionArray);
             optionArray.sort((a, b) => parseInt(a.value) - parseInt(b.value));
             optionArray.forEach(
-                element => document.getElementById("modDisplayFontSize").appendChild(element)
+                element => document.getElementById("fontSizeDropdownMenu").appendChild(element)
             )
         }
+        /* Slider: ㄋet value to computed font size of <body> */
+        document.getElementById("fontSizeSlider").value = parseInt(window.getComputedStyle(document.body).getPropertyValue("font-size"));
 
-    // Dropdown menu function:
-    document.getElementById("modDisplayFontSize").addEventListener("change", () => {
-        modDisplayFontSize(document.getElementById("modDisplayFontSize").value)
+    
+    // Dropdown Menu:
+    document.getElementById("fontSizeDropdownMenu").addEventListener("change", () => {
+        document.querySelectorAll("#fontDemo dd").forEach(
+            element => element.style.fontSize = document.getElementById("fontSizeDropdownMenu").value
+        )
+
+        /* Linking Slider */
+        document.getElementById("fontSizeSlider").value = parseInt(document.getElementById("fontSizeDropdownMenu").value);
+        dynamicOption.remove();
+        isOriginalMenu = true;
     });
 
-    function modDisplayFontSize(fontSizeOption) {
-        document.querySelectorAll("#fontDisplay dd").forEach(
-            element => element.style.fontSize = fontSizeOption
-        )
-    };
 
-    // Slide bar function:
+    // Slider:
+    // document.getElementById("fontSizeSlider").addEventListener("input", () => {
+        // document.querySelectorAll("#fontDemo dd").forEach(
+        //     element => element.style.fontSize = document.getElementById("fontSizeSlider").value.toString() + "px"
+        // )
+        
+        // let dynamicOption;
+
+        // (isOptionAvailable) ?
+        // {create new option element; appendChild to menu; sort the menu; } : {}
+        
+    // };    
+    
+    let isOriginalMenu = true;
+    let isOptionAvailable;
+    let dynamicOption;
+
+    document.getElementById("fontSizeSlider").addEventListener("input", () => {
+        document.querySelectorAll("#fontDemo dd").forEach(
+            element => element.style.fontSize = document.getElementById("fontSizeSlider").value.toString() + "px"
+        )
+        
+        /* if (isOriginalMenu === true) {
+            (isOptionAvailable) ? 
+            {select} : 
+            {create new option element; add it to menu; assign it to dynamicOption; sort the menu; isOriginalMenu = false}} */
+        if (isOriginalMenu === true) {
+            isOptionAvailable = false; // Setting default value.
+            document.querySelectorAll("#fontSizeDropdownMenu option").forEach(
+                element => {
+                    if (element.textContent === document.getElementById("fontSizeSlider").value.toString() + "px") {
+                        element.selected = true;
+                        isOptionAvailable = true;
+                    }
+                }
+            );
+            if (isOptionAvailable === false) {
+                dynamicOption = document.getElementById("fontSizeDropdownMenu").appendChild(document.createElement("option"));
+                dynamicOption.textContent = window.getComputedStyle(document.querySelector("#fontDemo dd")).getPropertyValue("font-size");
+                dynamicOption.selected = true;
+                let optionArray = Array.from(
+                    document.getElementById("fontSizeDropdownMenu").options
+                );
+                optionArray.sort((a, b) => parseInt(a.value) - parseInt(b.value));
+                optionArray.forEach(
+                    element => document.getElementById("fontSizeDropdownMenu").appendChild(element)
+                )
+                isOriginalMenu = false;
+            };
+        }
+
+        /* if (isOriginalMenu === false) {
+            (isOptionAvailable) ? {select} : {modify dynamicOption; sort the menu}} */
+        if (isOriginalMenu === false) {
+            isOptionAvailable = false; // Setting default value.
+            document.querySelectorAll("#fontSizeDropdownMenu option").forEach(
+                element => {
+                    if (element.textContent === document.getElementById("fontSizeSlider").value.toString() + "px") {
+                        element.selected = true;
+                        isOptionAvailable = true;
+                    }
+                }
+            );
+
+            if (isOptionAvailable === false) {
+                dynamicOption.textContent = window.getComputedStyle(document.querySelector("#fontDemo dd")).getPropertyValue("font-size");
+                dynamicOption.selected = true;
+                let optionArray = Array.from(
+                    document.getElementById("fontSizeDropdownMenu").options
+                );
+                optionArray.sort((a, b) => parseInt(a.value) - parseInt(b.value));
+                optionArray.forEach(
+                    element => document.getElementById("fontSizeDropdownMenu").appendChild(element)
+                )
+                isOptionAvailable = false;
+            };
+        }
+    });
